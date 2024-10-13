f479039453443847b287894a83731b71667aaa14
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-10-13 23:42:55 +0800

    Random number 1-100

--- Number_Guessing_Game.php
+++ Number_Guessing_Game.php
@@ -4,6 +4,8 @@ echo "Welcome to the Number Guessing Game!\n";
 echo "I have picked a random number between 1 and 100.\n";
 echo "Can you guess what it is?\n";
 
-
+// Generate a random number between 1 and 100
+$randomNumber = rand(1, 100);
+$attempts = 0;
 
 ?>
