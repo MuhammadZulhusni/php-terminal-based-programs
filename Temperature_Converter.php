2ecb6e8d8eb9d0daad2b7e840f97bf85239f3a52
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-11-25 12:41:05 +0800

    function  Kelvin to Fahrenheit

--- Temperature_Converter.php
+++ Temperature_Converter.php
@@ -24,4 +24,9 @@ function kelvinToCelsius($kelvin) {
     return $kelvin - 273.15;
 }
 
+// Function to convert Kelvin to Fahrenheit
+function kelvinToFahrenheit($kelvin) {
+    return ($kelvin - 273.15) * 9/5 + 32;
+}
+
 ?>
